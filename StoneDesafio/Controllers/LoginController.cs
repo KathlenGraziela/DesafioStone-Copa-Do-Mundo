57e59ce874930f393c87fe43b9a4b3272234ec26
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-10-16 12:11:51 -0300

    pasta de Login e de Layout adicionado dando início a tela de login. Sucess

---
+++ StoneDesafio/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoneDesafio.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
