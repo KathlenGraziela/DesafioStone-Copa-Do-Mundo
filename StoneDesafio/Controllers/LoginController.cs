3c8febd65eed78e8ec6b09eda607515be43c1054
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-10-16 13:10:21 -0300

    modificações e redirecionamento de login para a Home

--- StoneDesafio/Controllers/LoginController.cs
+++ StoneDesafio/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StoneDesafio.Models;
 
 namespace StoneDesafio.Controllers
 {
@@ -7,6 +8,25 @@ namespace StoneDesafio.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(Login login)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return View("Index");
+
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos realizar seu login, tente novamente. Detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
