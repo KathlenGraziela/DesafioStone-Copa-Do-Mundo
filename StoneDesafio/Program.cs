052f28629517d4579de6f622db13d925e1a3befc
ReArmel <rearmel@icloud.com>
2022-10-16 15:12:59 -0400

    Inclusao_Jogo

--- StoneDesafio/Program.cs
+++ StoneDesafio/Program.cs
@@ -51,6 +51,7 @@ if (app.Environment.IsDevelopment())
         
         var services = scope.ServiceProvider;
         var dbContext = services.GetRequiredService<AppDbContext>();
+        
         dbContext.Database.EnsureDeleted();
         dbContext.Database.EnsureCreated();
     }
