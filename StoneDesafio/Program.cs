6a509807fe0cd2b37a34521313ec764780852b5e
samuel <samuelp@lacunasoftware.com>
2022-10-18 18:59:59 -0300

    modificacoes

--- StoneDesafio/Program.cs
+++ StoneDesafio/Program.cs
@@ -53,8 +53,8 @@ if (app.Environment.IsDevelopment())
         var services = scope.ServiceProvider;
         var dbContext = services.GetRequiredService<AppDbContext>();
         
-       // dbContext.Database.EnsureDeleted();
-        dbContext.Database.EnsureCreated();
+       dbContext.Database.EnsureDeleted();
+       dbContext.Database.EnsureCreated();
     }
 
     app.UseHsts();
